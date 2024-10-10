1700a131f2461d49a528e8fe5c5d8b2c9a910145
André B Gorzoni <andreboolandim2@gmail.com>
2024-10-10 13:48:08 -0300

    feature btnAdc btnEdit btnRemov

--- controleDeCadastro/Form1.Designer.cs
+++ controleDeCadastro/Form1.Designer.cs
@@ -28,13 +28,200 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.dgvProdutos = new System.Windows.Forms.DataGridView();
+            this.btnAdc = new System.Windows.Forms.Button();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.btnRemov = new System.Windows.Forms.Button();
+            this.btnExpor = new System.Windows.Forms.Button();
+            this.txbNome = new System.Windows.Forms.TextBox();
+            this.txbID = new System.Windows.Forms.TextBox();
+            this.txbPreco = new System.Windows.Forms.TextBox();
+            this.lblNome = new System.Windows.Forms.Label();
+            this.lblPreco = new System.Windows.Forms.Label();
+            this.lblQtd = new System.Windows.Forms.Label();
+            this.txbQtd = new System.Windows.Forms.TextBox();
+            this.lblID = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProdutos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitulo.Location = new System.Drawing.Point(34, 12);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(387, 63);
+            this.lblTitulo.TabIndex = 0;
+            this.lblTitulo.Text = "Cadastro de Produtos";
+            this.lblTitulo.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // dgvProdutos
+            // 
+            this.dgvProdutos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvProdutos.Location = new System.Drawing.Point(458, 12);
+            this.dgvProdutos.Name = "dgvProdutos";
+            this.dgvProdutos.Size = new System.Drawing.Size(780, 670);
+            this.dgvProdutos.TabIndex = 1;
+            // 
+            // btnAdc
+            // 
+            this.btnAdc.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdc.Location = new System.Drawing.Point(12, 364);
+            this.btnAdc.Name = "btnAdc";
+            this.btnAdc.Size = new System.Drawing.Size(204, 49);
+            this.btnAdc.TabIndex = 2;
+            this.btnAdc.Text = "Adicionar Item";
+            this.btnAdc.UseVisualStyleBackColor = true;
+            this.btnAdc.Click += new System.EventHandler(this.btnAdc_Click);
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEdit.Location = new System.Drawing.Point(236, 364);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(204, 49);
+            this.btnEdit.TabIndex = 3;
+            this.btnEdit.Text = "Editar Item";
+            this.btnEdit.UseVisualStyleBackColor = true;
+            this.btnEdit.Click += new System.EventHandler(this.btnEdit_Click);
+            // 
+            // btnRemov
+            // 
+            this.btnRemov.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRemov.Location = new System.Drawing.Point(12, 431);
+            this.btnRemov.Name = "btnRemov";
+            this.btnRemov.Size = new System.Drawing.Size(204, 49);
+            this.btnRemov.TabIndex = 4;
+            this.btnRemov.Text = "Remover Item";
+            this.btnRemov.UseVisualStyleBackColor = true;
+            this.btnRemov.Click += new System.EventHandler(this.btnRemov_Click);
+            // 
+            // btnExpor
+            // 
+            this.btnExpor.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExpor.Location = new System.Drawing.Point(12, 633);
+            this.btnExpor.Name = "btnExpor";
+            this.btnExpor.Size = new System.Drawing.Size(204, 49);
+            this.btnExpor.TabIndex = 5;
+            this.btnExpor.Text = "Exportar Lista";
+            this.btnExpor.UseVisualStyleBackColor = true;
+            // 
+            // txbNome
+            // 
+            this.txbNome.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txbNome.Location = new System.Drawing.Point(13, 168);
+            this.txbNome.Name = "txbNome";
+            this.txbNome.Size = new System.Drawing.Size(427, 31);
+            this.txbNome.TabIndex = 6;
+            // 
+            // txbID
+            // 
+            this.txbID.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txbID.Location = new System.Drawing.Point(294, 245);
+            this.txbID.Name = "txbID";
+            this.txbID.ReadOnly = true;
+            this.txbID.Size = new System.Drawing.Size(146, 31);
+            this.txbID.TabIndex = 9;
+            // 
+            // txbPreco
+            // 
+            this.txbPreco.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txbPreco.Location = new System.Drawing.Point(13, 245);
+            this.txbPreco.Name = "txbPreco";
+            this.txbPreco.Size = new System.Drawing.Size(111, 31);
+            this.txbPreco.TabIndex = 7;
+            // 
+            // lblNome
+            // 
+            this.lblNome.AutoSize = true;
+            this.lblNome.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNome.Location = new System.Drawing.Point(12, 145);
+            this.lblNome.Name = "lblNome";
+            this.lblNome.Size = new System.Drawing.Size(51, 20);
+            this.lblNome.TabIndex = 11;
+            this.lblNome.Text = "Nome";
+            // 
+            // lblPreco
+            // 
+            this.lblPreco.AutoSize = true;
+            this.lblPreco.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPreco.Location = new System.Drawing.Point(12, 222);
+            this.lblPreco.Name = "lblPreco";
+            this.lblPreco.Size = new System.Drawing.Size(50, 20);
+            this.lblPreco.TabIndex = 12;
+            this.lblPreco.Text = "Preço";
+            // 
+            // lblQtd
+            // 
+            this.lblQtd.AutoSize = true;
+            this.lblQtd.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblQtd.Location = new System.Drawing.Point(152, 222);
+            this.lblQtd.Name = "lblQtd";
+            this.lblQtd.Size = new System.Drawing.Size(35, 20);
+            this.lblQtd.TabIndex = 13;
+            this.lblQtd.Text = "Qtd";
+            // 
+            // txbQtd
+            // 
+            this.txbQtd.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txbQtd.Location = new System.Drawing.Point(153, 245);
+            this.txbQtd.Name = "txbQtd";
+            this.txbQtd.Size = new System.Drawing.Size(111, 31);
+            this.txbQtd.TabIndex = 9;
+            // 
+            // lblID
+            // 
+            this.lblID.AutoSize = true;
+            this.lblID.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblID.Location = new System.Drawing.Point(292, 222);
+            this.lblID.Name = "lblID";
+            this.lblID.Size = new System.Drawing.Size(26, 20);
+            this.lblID.TabIndex = 15;
+            this.lblID.Text = "ID";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1250, 694);
+            this.Controls.Add(this.lblID);
+            this.Controls.Add(this.txbQtd);
+            this.Controls.Add(this.lblQtd);
+            this.Controls.Add(this.lblPreco);
+            this.Controls.Add(this.lblNome);
+            this.Controls.Add(this.txbPreco);
+            this.Controls.Add(this.txbID);
+            this.Controls.Add(this.txbNome);
+            this.Controls.Add(this.btnExpor);
+            this.Controls.Add(this.btnRemov);
+            this.Controls.Add(this.btnEdit);
+            this.Controls.Add(this.btnAdc);
+            this.Controls.Add(this.dgvProdutos);
+            this.Controls.Add(this.lblTitulo);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProdutos)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.DataGridView dgvProdutos;
+        private System.Windows.Forms.Button btnAdc;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.Button btnRemov;
+        private System.Windows.Forms.Button btnExpor;
+        private System.Windows.Forms.TextBox txbNome;
+        private System.Windows.Forms.TextBox txbID;
+        private System.Windows.Forms.TextBox txbPreco;
+        private System.Windows.Forms.Label lblNome;
+        private System.Windows.Forms.Label lblPreco;
+        private System.Windows.Forms.Label lblQtd;
+        private System.Windows.Forms.TextBox txbQtd;
+        private System.Windows.Forms.Label lblID;
     }
 }
 
--- controleDeCadastro/Form1.cs
+++ controleDeCadastro/Form1.cs
@@ -12,9 +12,130 @@ namespace controleDeCadastro
 {
     public partial class Form1 : Form
     {
+        DataTable produtosTable = new DataTable();
+
         public Form1()
         {
             InitializeComponent();
+
+            // Estrutura inicial do produtosTable
+            produtosTable.Columns.Add("ID", typeof(int));
+            produtosTable.Columns.Add("Nome", typeof(string));
+            produtosTable.Columns.Add("Preco", typeof(double));
+            produtosTable.Columns.Add("Qtd", typeof(int));
+
+            // Vincular o DataTable ao DataGridView
+            dgvProdutos.DataSource = produtosTable;
+
+            // Chave primária (Id) automática
+            produtosTable.PrimaryKey = new DataColumn[] { produtosTable.Columns["ID"] };
+
+            // Desabilitar a adição automática de uma linha vazia
+            dgvProdutos.AllowUserToAddRows = false;
+
+            // Adicionar valores predefinidos
+            produtosTable.Rows.Add(1, "Produto A", 10.0, 5);
+            produtosTable.Rows.Add(2, "Produto B", 15.0, 8);
+            produtosTable.Rows.Add(3, "Produto C", 20.0, 3);
+        }
+
+        // Adicionar dados
+        private void btnAdc_Click(object sender, EventArgs e)
+        {
+            // Verificar se todos os campos estão preenchidos
+            if (string.IsNullOrWhiteSpace(txbNome.Text) ||
+                string.IsNullOrWhiteSpace(txbPreco.Text) ||
+                string.IsNullOrWhiteSpace(txbQtd.Text))
+            {
+                MessageBox.Show("Por favor, preencha todos os campos antes de adicionar.", "Campos Vazios", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return; // Interrompe o processo de adição caso algum campo esteja vazio
+            }
+
+            try
+            {
+                // Gerar um novo ID automático
+                int novoID = produtosTable.Rows.Count > 0 ? Convert.ToInt32(produtosTable.Compute("MAX(ID)", "")) + 1 : 1;
+
+                // Adicionar a nova linha ao DataTable
+                produtosTable.Rows.Add(novoID, txbNome.Text, Convert.ToDouble(txbPreco.Text), Convert.ToInt32(txbQtd.Text));
+
+                // Limpar os campos de texto
+                txbNome.Clear();
+                txbPreco.Clear();
+                txbQtd.Clear();
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Por favor, insira valores numéricos válidos para o preço e quantidade.", "Erro de Formato", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnEdit_Click(object sender, EventArgs e)
+        {
+            if (dgvProdutos.SelectedRows.Count > 0)
+            {
+                // Linha selecionada
+                int selectedID = Convert.ToInt32(dgvProdutos.SelectedRows[0].Cells["ID"].Value);
+
+                // Encontrar a linha correspondente no DataTable
+                DataRow row = produtosTable.Rows[selectedID - 1];
+
+                if (row != null)
+                {
+                    // Atualizar os valores
+                    row["Nome"] = txbNome.Text;
+                    row["Preco"] = Convert.ToDouble(txbPreco.Text);
+                    row["Qtd"] = Convert.ToInt32(txbQtd.Text);
+                }
+
+                // Limpar os campos de texto
+                txbNome.Clear();
+                txbPreco.Clear();
+                txbQtd.Clear();
+            }
+            else
+            {
+                MessageBox.Show("Selecione um produto para editar.");
+            }
+        }
+
+        // Remover dados
+        private void btnRemov_Click(object sender, EventArgs e)
+        {
+            if (dgvProdutos.SelectedRows.Count > 0)
+            {
+                // Linha selecionada
+                int selectedID = Convert.ToInt32(dgvProdutos.SelectedRows[0].Cells["ID"].Value);
+
+                // Encontrar e remover a linha correspondente no DataTable
+                DataRow row = produtosTable.Rows.Find(selectedID);
+                if (row != null)
+                {
+                    produtosTable.Rows.Remove(row);
+                }
+
+                // Limpar os campos de texto
+                txbNome.Clear();
+                txbPreco.Clear();
+                txbQtd.Clear();
+            }
+            else
+            {
+                MessageBox.Show("Selecione um produto para editar.");
+            }
+        }
+
+        // Selecionar dados no DataGridView para editar
+        private void dataGridView1_SelectionChanged(object sender, EventArgs e)
+        {
+            if (dgvProdutos.SelectedRows.Count > 0)
+            {
+                // Preencher os campos de texto com os dados selecionados
+                txbNome.Text = dgvProdutos.SelectedRows[0].Cells["Nome"].Value.ToString();
+                txbPreco.Text = dgvProdutos.SelectedRows[0].Cells["Preco"].Value.ToString();
+                txbQtd.Text = dgvProdutos.SelectedRows[0].Cells["Qtd"].Value.ToString();
+                txbID.Text = dgvProdutos.SelectedRows[0].Cells["ID"].Value.ToString();
+            }
         }
     }
 }
